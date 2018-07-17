f056f8dbb3cb872d2b20ff7204f161e6940626f3
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-17 01:31:11 -0400

    Added constructor and set the courseId to state

--- src/containers/CourseEditor.js
+++ src/containers/CourseEditor.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class CourseEditor extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.selectCourse = this.selectCourse.bind(this);
+        this.state = {courseId: ''};
+    }
+
+    selectCourse(courseId) {
+        this.setState({courseId: courseId});
+    }
+
     render() {
         return (
-            <h3>Course Editor</h3>
-        )
+            <h3>Course {this.state.courseId}
+                </h3>)
+    }
+
+    componentDidMount() {
+        this.selectCourse (this.props.match.params.courseId);
+    }
+
+    componentWillReceiveProps(newProps){
+        this.selectCourse(newProps.match.params.courseId);
     }
 }
 
