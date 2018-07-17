e7ad06de1a1b2608bc1412dcb12dda9d2aff8a26
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-17 00:06:07 -0400

    Created the row component for course

---
+++ src/components/CourseRow.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class CourseRow extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <tr>
+                <td>{this.props.course.title}</td>
+                <td>
+                    <button onClick={() =>
+                                        {this.props.delete(this.props.course.id)}}>
+                        Delete
+                    </button>
+
+                </td>
+            </tr>)
+    }
+}
+
+export default CourseRow;
