bf452a63d617591f40ba2b0098ea0b1dedf63cd8
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-17 01:20:54 -0400

    Added routes to courselist and course editor pages

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import CourseList from './CourseList';
+import CourseEditor from './CourseEditor';
 
 class CourseManager extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Course Manager</h1>
-                <CourseList/>
-            </div>
+            <Router>
+                <div>
+                    <h1>Course Manager</h1>
+                    <Route path="/course"
+                           component={CourseList}>
+                    </Route>
+                    <Route path="/course/:courseId/edit"
+                           component={CourseEditor}>
+                    </Route>
+                </div>
+            </Router>
         )
     }
 }
