9cb9fc0e76c816f3ea55804d0467cdf3a7f9a06e
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-17 01:14:56 -0400

    Created course editor

---
+++ src/containers/CourseEditor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CourseEditor extends React.Component {
+    render() {
+        return (
+            <h3>Course Editor</h3>
+        )
+    }
+}
+
+export default CourseEditor;
