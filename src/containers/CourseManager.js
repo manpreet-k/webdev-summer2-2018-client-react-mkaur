370ce37b068c7d146da07cc80932f589dde262ca
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-17 01:07:42 -0400

    Added Course manager

---
+++ src/containers/CourseManager.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CourseList from './CourseList';
+
+class CourseManager extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Course Manager</h1>
+                <CourseList/>
+            </div>
+        )
+    }
+}
+
+export default CourseManager;
