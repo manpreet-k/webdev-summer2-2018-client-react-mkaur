8d5f92059709589ead070f78063f0371d85650a6
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-17 22:52:55 -0400

    Created the module list item

---
+++ src/components/ModuleListItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default class ModuleListItem extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render() {
+        return (
+            <Link to={`/course/${this.props.courseId}/module/${this.props.module.id}`}>
+                {this.props.module.title}
+                <span className='float-right'>
+                <i className="fa fa-trash"
+                   onClick={() => {
+                       this.props.delete(this.props.module.id)
+                   }}
+                />
+                <i className="fa fa-pencil"
+                   onClick={() => {
+                       this.props.update(this.props.module.id, this.props.module)
+                   }}
+                />
+                </span>
+            </Link>);
+    }
+}
