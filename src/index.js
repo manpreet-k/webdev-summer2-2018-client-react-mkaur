e2cb04b3fd19089a3cdee1ecdcfccf7e6f3e3da2
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-27 14:44:49 -0400

    Added store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
+import './index.css';
 import CourseManager from './containers/CourseManager';
-import './index.css'
+import {WidgetReducer} from "./reducers/WidgetReducer";
 
+let store = createStore(WidgetReducer)
 
 ReactDOM.render(
-    <CourseManager/>,
+    <Provider store={store}>
+        <CourseManager/>
+    </Provider>,
     document.getElementById('root')
 );
