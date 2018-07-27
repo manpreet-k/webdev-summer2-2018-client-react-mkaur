1ae7179ca1a6406938cb4a998689d65551065656
Manpreet Kaur <lnu.m@husky.neu.edu>
2018-07-27 14:42:18 -0400

    Added constants and actions for the reducers :

---
+++ src/actions/WidgetActions.js
@@ -0,0 +1,28 @@
+import WidgetServiceClient from "../services/WidgetServiceClient";
+import * as constants from '../constants/WidgetConstants';
+
+export const findAllWidgetsForTopic = (dispatch, topicId) => {
+    if ('' !== topicId) {
+        WidgetServiceClient.instance
+            .findAllWidgetsForTopic(topicId)
+            .then(widgets => dispatch({
+                type: constants.FIND_ALL_WIDGETS_FOR_TOPIC,
+                widgets: widgets
+            }))
+            .catch((e) => console.log(e))
+    }
+
+};
+
+export const saveAllWidgets = (topicId, widgets) => {
+    let widgetService = WidgetServiceClient.instance;
+    if ('' === topicId) {
+        return [];
+    }
+        widgets.map((widget) =>
+            widgetService.createWidget(topicId, widget)
+                .then(function (widgets) {
+                    return widgets;
+                })
+        )
+};
